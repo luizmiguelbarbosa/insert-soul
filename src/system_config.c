d3110460ab06f3e3a111d89bc4546ff7faac3112
Gustavo Charamba <gustavocharamba@Gustavos-MacBook-Air.local>
2025-11-27 10:38:59 -0300

    mac/win/linux

--- src/system_config.c
+++ src/system_config.c
@@ -3,8 +3,6 @@
 #if defined(_WIN32) || defined(_WIN64)
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-#undef PlaySound
-#undef DrawTextEx
 
 void getScreenResolution(int *width, int *height) {
     *width  = GetSystemMetrics(SM_CXSCREEN);
@@ -14,7 +12,7 @@ void getScreenResolution(int *width, int *height) {
 void getSystemLanguage(char *lang, int size) {
     LANGID langId = GetUserDefaultUILanguage();
     DWORD ret = GetLocaleInfo(langId, LOCALE_SISO639LANGNAME, lang, size);
-    if (ret == 0) lang[0] = '\0'; // fallback
+    if (ret == 0) lang[0] = '\0';
 }
 
 #elif defined(__linux__)
@@ -30,7 +28,7 @@ void getScreenResolution(int *width, int *height) {
         *height = s->height;
         XCloseDisplay(d);
     } else {
-        *width  = 800;  // fallback
+        *width = 800;
         *height = 600;
     }
 }
@@ -38,8 +36,29 @@ void getScreenResolution(int *width, int *height) {
 void getSystemLanguage(char *lang, int size) {
     const char *locale = setlocale(LC_ALL, "");
     if (locale) {
-        strncpy(lang, locale, size-1);
-        lang[size-1] = '\0';
+        strncpy(lang, locale, size - 1);
+        lang[size - 1] = '\0';
+    } else {
+        strncpy(lang, "en", size);
+    }
+}
+
+#elif defined(__APPLE__)
+#include <CoreGraphics/CoreGraphics.h>
+#include <locale.h>
+#include <string.h>
+
+void getScreenResolution(int *width, int *height) {
+    CGRect rect = CGDisplayBounds(CGMainDisplayID());
+    *width = rect.size.width;
+    *height = rect.size.height;
+}
+
+void getSystemLanguage(char *lang, int size) {
+    const char *locale = setlocale(LC_ALL, "");
+    if (locale) {
+        strncpy(lang, locale, size - 1);
+        lang[size - 1] = '\0';
     } else {
         strncpy(lang, "en", size);
     }
